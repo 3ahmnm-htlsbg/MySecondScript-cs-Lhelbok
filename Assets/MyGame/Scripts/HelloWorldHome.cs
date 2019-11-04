316ddcdc92d20fcfb728cf1ad7adadbb8f2aca55
LH03 <lea.helbok03@htl-salzburg.ac.at>
2019-11-04 11:39:43 +0100

    added first line of code to the script

--- Assets/MyGame/Scripts/HelloWorldHome.cs
+++ Assets/MyGame/Scripts/HelloWorldHome.cs
@@ -7,9 +7,8 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World at home");
     }
-
     // Update is called once per frame
     void Update()
     {
